478e3992ce95d6176c9dc8d09e556b0cd362ec7b
JoshuaHowell <howjosh@gmail.com>
2018-09-05 11:55:05 -0500

    JH -- Written At -- Replace written at with at mark.

--- src/main/java/EmailFinder.java
+++ src/main/java/EmailFinder.java
@@ -1,5 +1,33 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class EmailFinder {
     public String findEmail(String perspectiveEmail) {
+
+        /*
+            1. Any number of letters
+            2. @ or (at)
+            3. Any number of letters
+            4. .edu
+         */
+        String regularExpression ="[a-z]*(\\(at\\)|@)cse.tamu.edu";
+        Pattern pattern = Pattern.compile(regularExpression);
+        Matcher matcher = pattern.matcher(perspectiveEmail);
+
+        if (matcher.matches()) {
+            for (int i = 0; i <= matcher.groupCount(); i++) {
+                System.out.println(i + " --> " + matcher.group(i));
+                //System.out.println(matcher.group(i));
+            }
+
+            perspectiveEmail = perspectiveEmail.replaceAll("\\(at\\)", "@");
+        }
+
+
+
+
+
+
         return perspectiveEmail;
     }
 }
--- src/test/java/EmailFinderTest.java
+++ src/test/java/EmailFinderTest.java
@@ -4,12 +4,21 @@ import org.junit.Test;
 public class EmailFinderTest {
 
     @Test
-    public void shouldEmailWhenAlreadyFormattedProperly(){
+    public void shouldReturnEmailWhenAlreadyFormattedProperly(){
         EmailFinder emailFinder = new EmailFinder();
         String expectedEmail = "huangrh@cse.tamu.edu";
 
         String returnedEmail = emailFinder.findEmail("huangrh@cse.tamu.edu");
 
+        Assert.assertEquals(expectedEmail, returnedEmail);
+    }
+
+    @Test
+    public void shouldReturnEmailWhenTheWordAtIsUsedInsteadofAnAtMark(){
+        EmailFinder emailFinder = new EmailFinder();
+        String expectedEmail = "huangrh@cse.tamu.edu";
+
+        String returnedEmail = emailFinder.findEmail("huangrh(at)cse.tamu.edu");
 
         Assert.assertEquals(expectedEmail, returnedEmail);
     }
