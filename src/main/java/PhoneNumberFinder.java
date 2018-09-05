b778507886ff941c2f524e23e1f173dd2c814faa
JoshuaHowell <howjosh@gmail.com>
2018-09-05 16:08:11 -0500

    JH -- Embedded Phone Number -- Match against a phone number embedded in HTML.

--- src/main/java/PhoneNumberFinder.java
+++ src/main/java/PhoneNumberFinder.java
@@ -5,10 +5,11 @@ public class PhoneNumberFinder {
     public String findPhoneNumber(String perspectivePhoneNumber) {
         String reconstructedPhoneNumber = "";
 
-
+        String countryCodeRegEx = "\\(?\\+[\\d]{1,3}\\)?";
         String areaCodeRegEx = "\\(?([\\d]{3})\\)?";
-        String phoneNumberRegEx = "([\\d]{3}-[\\d]{4})";
-        String countryCodeRegEx = "\\(\\+[\\d]{1,3}\\)";
+        String firstPartOfPhoneNumber = "[\\d]{3}";
+        String secondPartOfPhoneNumber = "[\\d]{4}";
+        String phoneNumberRegEx = "(" + firstPartOfPhoneNumber + "-" + secondPartOfPhoneNumber + ")";
         String prefixRegEx = "(Phone|Tel " + countryCodeRegEx + ")";
 
         String regularExpression = prefixRegEx + ":\\s"+ areaCodeRegEx + "[\\s|-]" + phoneNumberRegEx;
@@ -27,7 +28,22 @@ public class PhoneNumberFinder {
             return reconstructedPhoneNumber;
         }
 
+        String anotherRegularExpression = "<a href=\"contact.html\">TEL</a> " + countryCodeRegEx + "&thinsp;(" + areaCodeRegEx + ")&thinsp;(" + firstPartOfPhoneNumber + ")&thinsp;(" + secondPartOfPhoneNumber + ")";
 
+
+        pattern = Pattern.compile(anotherRegularExpression);
+        matcher = pattern.matcher(perspectivePhoneNumber);
+
+        if (matcher.matches()) {
+            for (int i = 0; i <= matcher.groupCount(); i++) {
+                System.out.println(i + " --> " + matcher.group(i));
+                System.out.println(matcher.group(i));
+            }
+
+            reconstructedPhoneNumber =  matcher.group(2) + "-" + matcher.group(3) + "-" + matcher.group(4);
+
+            return reconstructedPhoneNumber;
+        }
 
 
         return reconstructedPhoneNumber;
--- src/test/java/PhoneNumberFinderTest.java
+++ src/test/java/PhoneNumberFinderTest.java
@@ -24,4 +24,14 @@ public class PhoneNumberFinderTest {
 
         Assert.assertEquals(expectedPhoneNumber, returnedPhoneNumber);
     }
+
+    @Test
+    public void shouldReturnPhoneNumberWhenEmbeddedHTML(){
+        String expectedPhoneNumber = "979-862-2908";
+        PhoneNumberFinder phoneNumberFinder = new PhoneNumberFinder();
+
+        String returnedPhoneNumber = phoneNumberFinder.findPhoneNumber("<a href=\"contact.html\">TEL</a> +1&thinsp;979&thinsp;862&thinsp;2908");
+
+        Assert.assertEquals(expectedPhoneNumber, returnedPhoneNumber);
+    }
 }
