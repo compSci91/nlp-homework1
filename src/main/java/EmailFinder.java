85206ed5c2b5e69fad7fc171cab8645c0a269257
JoshuaHowell <howjosh@gmail.com>
2018-09-05 13:11:59 -0500

    JH -- Written At -- Find the written word 'at' when their are no parenthesis.

--- src/main/java/EmailFinder.java
+++ src/main/java/EmailFinder.java
@@ -6,21 +6,22 @@ public class EmailFinder {
 
         /*
             1. Any number of letters
-            2. @ or (at)
+            2. @ or (at) or _at_
             3. Any number of letters
             4. .edu
          */
-        String regularExpression ="[a-z]*(\\(at\\)|@)cse.tamu.edu";
+        String regularExpression ="[a-z]*(\\(at\\)|\\sat\\s|@)cse\\.tamu\\.edu";
         Pattern pattern = Pattern.compile(regularExpression);
         Matcher matcher = pattern.matcher(perspectiveEmail);
 
         if (matcher.matches()) {
             for (int i = 0; i <= matcher.groupCount(); i++) {
                 System.out.println(i + " --> " + matcher.group(i));
-                //System.out.println(matcher.group(i));
+                System.out.println(matcher.group(i));
             }
 
             perspectiveEmail = perspectiveEmail.replaceAll("\\(at\\)", "@");
+            perspectiveEmail = perspectiveEmail.replaceAll(" at ", "@");
         }
 
 
--- src/test/java/EmailFinderTest.java
+++ src/test/java/EmailFinderTest.java
@@ -22,4 +22,23 @@ public class EmailFinderTest {
 
         Assert.assertEquals(expectedEmail, returnedEmail);
     }
+
+    @Test
+    public void shouldReturnEmailWhenTheWordAtIsUsedWithoutParentheisInsteadOfAnAtMark(){
+        EmailFinder emailFinder = new EmailFinder();
+        String expectedEmail = "huangrh@cse.tamu.edu";
+
+        String returnedEmail = emailFinder.findEmail("huangrh at cse.tamu.edu");
+
+        Assert.assertEquals(expectedEmail, returnedEmail);
+    }
+
+    public void shouldReturnEmailWhenAllSymbolsHaveBeenTurnedIntoWords(){
+        EmailFinder emailFinder = new EmailFinder();
+        String expectedEmail = "huangrh@cse.tamu.edu";
+
+        String returnedEmail = emailFinder.findEmail("huangrh at cse dot tamu dot edu");
+
+        Assert.assertEquals(expectedEmail, returnedEmail);
+    }
 }
