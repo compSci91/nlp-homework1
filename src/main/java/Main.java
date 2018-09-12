394f2909c5fd03c1176e2aa7d94a167bda0b14be
JoshuaHowell <howjosh@gmail.com>
2018-09-12 14:47:35 -0500

    JH -- Read From File

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,29 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
+    public static void main(String[] args) {
+
+        File file = new File("/Users/JoshuaHowell/Desktop/Texas A&M/Year 2/Fall 2018/Natural Language Processing/Homework 1/PA1-638/data_dev/dev/ashishg");
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        while (sc.hasNextLine()) {
+            System.out.println(sc.nextLine());
+        }
+
+
+    }
+
     public static List<String> getAllMatches(String text, String regex) {
         List<String> matches = new ArrayList<String>();
         Matcher m = Pattern.compile("(?=(" + regex + "))").matcher(text);
@@ -13,8 +33,5 @@ public class Main {
         return matches;
     }
 
-    public static void main(String[] args) {
-        System.out.println(getAllMatches("abaca", "a.a"));
-        System.out.println(getAllMatches("abaa", "a.*a"));
-    }
+
 }
